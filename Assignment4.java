6c6130c5fb117a9c9f5ec1890b4ea3254613e6fb
sumit2k7 <sumit2k7@users.noreply.github.com>
2016-04-19 16:32:55 +0530

    Added files via upload

---
+++ Assignment4.java
@@ -0,0 +1,16 @@
+
+public class Assignment4 {
+	public static void main (String[] args)
+	{
+		int x=5;
+		int y=10;
+		
+		System.out.println("(a) x+y*2 -> "+(x+y*2));
+		System.out.println("(b) x-y*2 -> "+ (x-y*2));
+		System.out.println("(c) (x+y)*2 -> "+((x+y)*2));
+		System.out.println("(d) y%x -> "+(y%x));
+		
+		
+	}
+
+}
